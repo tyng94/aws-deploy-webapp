af0e7c7c9b0afc429c226e30a6dccb8363c53fba
Tze Yang Ng <tzeyang@dsaid.gov.sg>
2023-11-19 11:28:16 +0800

    database

--- app_runner/startup.sh
+++ app_runner/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 app_runner.wsgi
+python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 app_runner.wsgi
